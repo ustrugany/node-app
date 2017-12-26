e5d2e59f8d408517486acf3ee4124df6681b2e8f
ustrugany <piotr@propertyfinder.ae>
2017-12-26 23:11:52 +0400

    make use of env variable

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
 # !/bin/sh
-docker run -i -p 192.168.99.100:8888:8888 -v $(pwd):/tmp -w /tmp --rm node:latest $@
+docker run -i -p $(docker-machine ip):8888:8888 -v $(pwd):/tmp -w /tmp --rm node:latest $@
